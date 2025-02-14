83c73f6ea975452cc1918a6ae16644e8171f0598
tetu5473 <tetu5473@gmail.com>
2025-02-14 21:41:13 +0900

    2025.02.14

--- src/todo.tsx
+++ src/todo.tsx
@@ -1,31 +1,66 @@
 import React, { useState } from 'react';
 
+
+// "Todo" 型の定義をコンポーネント外で行います
+type Todo = {
+  content: string; // プロパティ content は文字列型
+};
+
+
 // Todo コンポーネントの定義
 const Todo: React.FC = () => {
-  const [todo, setTodo] = useState('Hello, React!');
+  const [todos, setTodos] = useState<Todo[]>([]); // Todoの配列を保持するステート
+  const [text, setText] = useState(''); // フォーム入力のためのステート
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // 追加処理をここに追加します
-    console.log('Todo added:', todo);
-    setTodo(''); // フォームをリセット
+
+  // todos ステートを更新する関数
+  const handleSubmit = () => {
+    // 何も入力されていなかったらリターン
+    if (!text) return;
+
+
+    // 新しい Todo を作成
+    const newTodo: Todo = {
+      content: text, // text ステートの値を content プロパティへ
+    };
+
+
+    /**
+     * 更新前の todos ステートを元に
+     * スプレッド構文で展開した要素へ
+     * newTodo を加えた新しい配列でステートを更新
+     **/
+    setTodos((prevTodos) => [newTodo, ...prevTodos]);
+
+
+    // フォームへの入力をクリアする
+    setText('');
   };
+
 
   return (
     <div>
-          <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // フォームのデフォルト動作を防ぐ
+          handleSubmit(); // handleSubmit 関数を呼び出す
+        }}
+      >
         <input
           type="text"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
+          value={text} // フォームの入力値をステートにバインド
+          onChange={(e) => setText(e.target.value)} // 入力値が変わった時にステートを更新
         />
-        <input
-          type="submit"
-          value="追加"
-        />
+        <input type="submit" content="追加" /> {/* ボタンをクリックしてもonSubmitをトリガーしない */}
       </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo.content}</li> // todoのリストを表示
+        ))}
+      </ul>
     </div>
   );
 };
+
 
 export default Todo;
